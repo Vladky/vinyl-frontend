9f069afa54b6b6c96ffb3aae5f096ea0cd8b0fb0
vladky_94 <kvladky@gmail.com>
2020-09-17 00:44:59 +0300

    Update start screen

--- src/components/vinyl/index.ts
+++ src/components/vinyl/index.ts
@@ -1,2 +1,1 @@
-export { default as VinylSearchBox } from './VinylSearchBox.vue'
 export { default as VinylResultList } from './VinylResultList.vue'
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -3,13 +3,23 @@
     <div class="flex-grow-0">
       <v-form @submit.prevent="submit">
         <v-row class="justify-center" no-gutters>
-          <v-col cols="12" md="6" xl="4">
-            <vinyl-search-box :value.sync="searchText" />
+          <v-col cols="12" md="6" xl="4" align="center">
+            <v-text-field v-model="searchText" solo rounded clearable>
+              <template #prepend-inner v-if="defaultState">
+                <v-icon>mdi-magnify</v-icon>
+              </template>
+              <template #append v-if="!defaultState">
+                <v-btn icon type="submit">
+                  <v-icon>mdi-magnify</v-icon>
+                </v-btn>
+              </template>
+            </v-text-field>
+            <v-btn v-if="defaultState" rounded type="submit">Поиск пластинок</v-btn>
           </v-col>
         </v-row>
       </v-form>
     </div>
-    <div :class="['list-transition', { 'flex-grow-1': !!items.length || loading || noResult }]">
+    <div :class="['list-transition', { 'flex-grow-1': !defaultState }]">
       <vinyl-result-list v-bind="{ items, loading, noData, noResult }" ref="result" />
     </div>
   </div>
@@ -17,14 +27,14 @@
 
 <script lang="ts">
 import { Vue, Component, Prop } from 'vue-property-decorator'
-import { VinylSearchBox, VinylResultList } from '@/components'
+import { VinylResultList } from '@/components'
 import { VinylService } from '@/services'
 import { Vinyl } from '@/models'
 
 Component.registerHooks(['beforeRouteEnter', 'beforeRouteUpdate'])
 
 @Component<Home>({
-  components: { VinylSearchBox, VinylResultList },
+  components: { VinylResultList },
   beforeRouteEnter(to, from, next) {
     next(vm => {
       vm.searchText = to.query.search as string
@@ -47,6 +57,9 @@ export default class Home extends Vue {
   loading = false
   get noData() {
     return !(this.items.length && this.searchText)
+  }
+  get defaultState() {
+    return !this.items.length && !this.loading && !this.noResult
   }
   noResult = false
   // eslint-disable-next-line @typescript-eslint/no-explicit-any
